1a83d38683ed4b330508243d8ed850cbd4cd0589
RanolP <public.ranolp@gmail.com>
2022-08-03 16:24:46 +0900

    feat: add search bar (#7)

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,13 +1,33 @@
 import React from "react";
+import agent from "../../agent";
 import logo from "../../imgs/logo.png";
 
-const Banner = () => {
+const Banner = (props) => {
+  const onInput = (event) => {
+    let search = event.currentTarget.value;
+
+    if (search.length < 3) {
+      search = undefined;
+    }
+
+    props.onSearch(
+      search,
+      (page) => agent.Items.byTitle(search, page),
+      agent.Items.byTitle(search)
+    );
+  };
+
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
           <span id="get-part">A place to get</span>
+          <input
+            id="search-box"
+            placeholder="What is it that you truly desire?"
+            onInput={onInput}
+          />
           <span> the cool stuff.</span>
         </div>
       </div>
